ecf53810e070b9c94821f2095a0fab713cd308e2
phseguin <psegu239@mtroyal.ca>
2020-04-14 23:46:32 -0600

    initial commit

---
+++ includes/menu.php
@@ -0,0 +1,15 @@
+<?php 
+function printMenu() {
+    ?>
+    <h3>Copyright © 2020 Pascal Hamish Seguin</h3>
+    <div id="footerNav">
+        <label class="footerlinks">Home</label>
+        <label class="footerlinks">Education</label>
+        <label class="footerlinks">Work Experince</label>
+        <label class="footerlinks">Volunteer Experince</label>
+        <label class="footerlinks">Projects</label>
+        <label class="footerlinks">Contact</label>
+    </div>
+    <?php
+}
+?>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include 'includes/menu.php';
+include 'includes/footer.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+   <meta charset="utf-8"/>  
+    <title>Pascal Hamish Seguin</title>   
+
+   <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body id="body">
+   <div id="menu"><?=printMenu();?></div>
+   <div id="homepage" class="content"> </div>
+   <div id="education" class="content"></div>
+   <div id="volunteer_experince" class="content"></div>
+   <div id="work_experince" class="content"></div>
+   <div id="projects" class="content"></div>
+   <div id="footer"><?=printFooter()?></div>
+<!---<script src="js/assign1Script.js" ></script>-->
+
+</body>
+</html>
